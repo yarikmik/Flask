25cc923dbd3ad2bd6246c33aa9d9e788a9177faf
yarikmik <yarik.mik@gmail.com>
2022-06-24 23:12:50 +0700

    lesson_1

---
+++ flaskProject/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = '23458708b13vbbde464f546ty84fgh322'
--- flaskProject/models.py
+++ flaskProject/models.py
@@ -1,6 +1,11 @@
-from flaskProject import db
+from flaskProject import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
