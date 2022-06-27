1228bb99696ca4e332eb5991d2c1a8047f47e2de
yarikmik <yarik.mik@gmail.com>
2022-06-27 15:23:00 +0700

    lesson_2

--- flaskProject/__init__.py
+++ flaskProject/__init__.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flaskProject.config import Config
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 #  создание flask приложения рекомендуется выполнять именно в __init__.py
 def create_app():
     app = Flask(__name__)
     db.init_app(app)
+    bcrypt.init_app(app)
 
     #  регистрация блюпринта
     from flaskProject.main.routes import main
@@ -21,4 +24,9 @@ def create_app():
 
     #  регистрация логин менеджера
     login_manager.init_app(app)
+
+    #  регистрация блюпринта пользователей
+    from flaskProject.users.routes import users
+    app.register_blueprint(users)
+
     return app
